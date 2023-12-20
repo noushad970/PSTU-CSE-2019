2224278bbbe6474ba6438f00c554e24282f7d8ee
MD. NOUSHAD BHUIYAN <ug2102038@cse.pstu.ac.bd>
2023-12-21 02:14:11 +0600

    Add files via upload

---
+++ Java Practise w3resource/nine_34.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+class nine_34 {
+    public static void main(String[] args) {
+        Scanner s= new Scanner(System.in);
+        System.out.println("Enter array size: ");
+        int n=s.nextInt();
+        System.out.println("Enter array's elements: ");
+        int[] a= new int[n];
+        for(int i=0;i<n;i++)
+        a[i]=s.nextInt();
+        Set<Integer> set= new HashSet(); 
+        for(int i=0;i<n;i++)
+        set.add(a[i]);
+        //problem
+    }
+}
