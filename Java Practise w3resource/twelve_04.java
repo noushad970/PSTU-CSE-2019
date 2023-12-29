933ebca108729fc3bbbbcb2342ab99ee313a2082
MD. NOUSHAD BHUIYAN <ug2102038@cse.pstu.ac.bd>
2023-12-30 01:19:34 +0600

    Add files via upload

---
+++ Java Practise w3resource/twelve_04.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import java.util.StringTokenizer;
+public class twelve_04 {
+    public static void main(String[] args) throws Exception {
+    Scanner s= new Scanner(System.in);
+    System.out.println("Enter the first string : ");
+    String x1=s.nextLine();
+    System.out.println("Enter the second string : ");
+    String x2=s.nextLine();
+    String ans=x1+" "+x2;
+    System.out.println(ans);
+    
+}
+}
