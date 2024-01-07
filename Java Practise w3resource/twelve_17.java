e243ed83c024c831430172490eff2e633e94808e
MD. NOUSHAD BHUIYAN <ug2102038@cse.pstu.ac.bd>
2024-01-07 11:20:55 +0600

    Add files via upload

---
+++ Java Practise w3resource/twelve_17.java
@@ -0,0 +1,13 @@
+
+public class twelve_17
+{
+    
+    public static void main(String[] args) {
+        String str = "This is a sample string.";
+
+        char[] arr = new char[] { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
+        str.getChars(4, 10, arr, 2);
+        System.out.println("The char array equals \"" +
+            arr + "\"");
+    }
+}
