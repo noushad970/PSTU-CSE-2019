e0cbbb6955edd77a9cdc4eee69f692ceedd139fc
MD. NOUSHAD BHUIYAN <ug2102038@cse.pstu.ac.bd>
2024-01-11 01:02:02 +0600

    Add files via upload

---
+++ Java Practise w3resource/twelve_29.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class twelve_29
+{
+    
+    public static void main(String[] args) {
+    
+    //System.out.println(str);
+    
+    Scanner s=new Scanner(System.in);
+    char[] c= {'a', 'b', 'c', 'd', 'e'};
+    String s1=new String(c);
+
+    System.out.println(s1.toUpperCase());
+    
+}
+}
