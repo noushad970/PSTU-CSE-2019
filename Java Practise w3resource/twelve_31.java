41fcd01759ae6524f576a1bb0556a951d0d591a7
MD. NOUSHAD BHUIYAN <ug2102038@cse.pstu.ac.bd>
2024-01-17 04:44:51 +0600

    Add files via upload

---
+++ Java Practise w3resource/twelve_31.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class twelve_31
+{
+    
+    public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter a string :");
+    String str1=scanner.nextLine();
+    String str2=str1.trim();
+    System.out.println(str2);
+    
+    
+}
+}
